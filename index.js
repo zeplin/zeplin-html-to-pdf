cbb161258442dd678ae29fa15e4f68371fc1e46a
Doruk Güneş <dorukgunes94@gmail.com>
2020-01-31 11:04:01 +0300

    Feature/lambda node.js 10.x and node.js 12.x (#19)
    * Add dockerfile
    
    * Adapt to handle lambda nodejs 10&12 runtimes
    
    * Revert chmod to 644
    
    * Change font arial to roboto

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 process.env.PATH = `${process.env.PATH}:${process.env.LAMBDA_TASK_ROOT}`;
-
 const wkhtmltopdf = require("./utils/wkhtmltopdf");
 const errorUtil = require("./utils/error");
 
 exports.handler = function handler(event, context, callback) {
     if (!event.html) {
         const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html'.");
-
         callback(errorResponse);
         return;
     }
